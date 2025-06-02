93d244dbb3b1fa7aa6c6ff87827068411439a9a9
cyberhackstar <64746305+cyberhackstar@users.noreply.github.com>
2025-06-03 01:38:08 +0530

    Update BookingEventEntity.java

--- CarListnerService/src/main/java/com/carrental/CarListnerService/model/BookingEventEntity.java
+++ CarListnerService/src/main/java/com/carrental/CarListnerService/model/BookingEventEntity.java
@@ -1,5 +1,27 @@
 package com.carrental.CarListnerService.model;
 
-public class BookingEventEntity {
+package com.car.listener.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "booking_events")
+public class BookingEventEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long bookingId;
+    private Long carId;
+    private String customerName;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private double totalAmount;
 }
