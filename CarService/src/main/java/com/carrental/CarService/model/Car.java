cae4963228affabff934ba189643f8350c701c3f
cyberhackstar <64746305+cyberhackstar@users.noreply.github.com>
2025-06-03 01:27:03 +0530

    Update Car.java

--- CarService/src/main/java/com/carrental/CarService/model/Car.java
+++ CarService/src/main/java/com/carrental/CarService/model/Car.java
@@ -1,12 +1,19 @@
 package com.carrental.CarService.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Car {
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String model;
     private String brand;
+    private String model;
     private boolean available;
     private double pricePerDay;
-    // Getters, Setters, Constructors
 }
