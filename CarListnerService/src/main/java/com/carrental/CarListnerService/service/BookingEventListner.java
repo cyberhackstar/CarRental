31bf2adcb6682a53cdfd14ca31408a75cf198588
cyberhackstar <64746305+cyberhackstar@users.noreply.github.com>
2025-06-03 01:35:00 +0530

    Update BookingEventListner.java

--- CarListnerService/src/main/java/com/carrental/CarListnerService/service/BookingEventListner.java
+++ CarListnerService/src/main/java/com/carrental/CarListnerService/service/BookingEventListner.java
@@ -1,5 +1,19 @@
 package com.carrental.CarListnerService.service;
 
-public class BookingEventListner {
+import com.car.listener.dto.BookingEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
 
+@Slf4j
+@Component
+public class BookingEventListener {
+
+    @JmsListener(destination = "booking-events")
+    public void handleBookingEvent(BookingEvent event) {
+        log.info("🎯 Received Booking Event: {}", event);
+
+        // Example: store in DB, send email, update analytics
+        // For now, we just log
+    }
 }
