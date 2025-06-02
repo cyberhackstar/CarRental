a6b0446892790e8f5495874224c31b744f3745b6
cyberhackstar <64746305+cyberhackstar@users.noreply.github.com>
2025-06-03 01:31:41 +0530

    Update BookingEventProducer.java

--- CarService/src/main/java/com/carrental/CarService/messaging/BookingEventProducer.java
+++ CarService/src/main/java/com/carrental/CarService/messaging/BookingEventProducer.java
@@ -1,5 +1,19 @@
 package com.carrental.CarService.messaging;
 
+import com.car.service.dto.BookingEvent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
 public class BookingEventProducer {
 
+    private static final String QUEUE = "booking-events";
+
+    @Autowired
+    private JmsTemplate jmsTemplate;
+
+    public void sendBookingEvent(BookingEvent event) {
+        jmsTemplate.convertAndSend(QUEUE, event);
+    }
 }
