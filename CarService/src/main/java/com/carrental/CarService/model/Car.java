62a70bbbf36317338a999d3cbcc80aa184a2ecdb
cyberhackstar <64746305+cyberhackstar@users.noreply.github.com>
2025-06-03 01:24:42 +0530

    Update Car.java

--- CarService/src/main/java/com/carrental/CarService/model/Car.java
+++ CarService/src/main/java/com/carrental/CarService/model/Car.java
@@ -1,5 +1,12 @@
 package com.carrental.CarService.model;
 
+@Entity
 public class Car {
-
+    @Id @GeneratedValue
+    private Long id;
+    private String model;
+    private String brand;
+    private boolean available;
+    private double pricePerDay;
+    // Getters, Setters, Constructors
 }
