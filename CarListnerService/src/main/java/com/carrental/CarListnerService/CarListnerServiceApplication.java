1a79a127b0e7f260d6c10b60c51ad33e937bf18e
cyberhackstar <64746305+cyberhackstar@users.noreply.github.com>
2025-06-03 01:36:00 +0530

    Update CarListnerServiceApplication.java

--- CarListnerService/src/main/java/com/carrental/CarListnerService/CarListnerServiceApplication.java
+++ CarListnerService/src/main/java/com/carrental/CarListnerService/CarListnerServiceApplication.java
@@ -2,8 +2,10 @@ package com.carrental.CarListnerService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
+@EnableJms
 public class CarListnerServiceApplication {
 
 	public static void main(String[] args) {
