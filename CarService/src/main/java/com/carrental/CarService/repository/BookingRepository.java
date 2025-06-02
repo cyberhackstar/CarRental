bbc9a4f65851e34939acf6bfb1c3bb5a36d2f668
cyberhackstar <64746305+cyberhackstar@users.noreply.github.com>
2025-06-03 01:28:31 +0530

    Update BookingRepository.java

--- CarService/src/main/java/com/carrental/CarService/repository/BookingRepository.java
+++ CarService/src/main/java/com/carrental/CarService/repository/BookingRepository.java
@@ -1,9 +1,8 @@
 package com.carrental.CarService.repository;
 
-import com.car.service.model.Car;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
 
-public interface CarRepository extends JpaRepository<Car, Long> {
-    List<Car> findByAvailableTrue();
+import com.car.service.model.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {
 }
