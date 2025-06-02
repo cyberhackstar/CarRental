34b33c53c86301d5722ce15b5a1ec17cf56b7fae
cyberhackstar <64746305+cyberhackstar@users.noreply.github.com>
2025-06-03 01:34:35 +0530

    Update BookingEvent.java

--- CarListnerService/src/main/java/com/carrental/CarListnerService/dto/BookingEvent.java
+++ CarListnerService/src/main/java/com/carrental/CarListnerService/dto/BookingEvent.java
@@ -1,5 +1,17 @@
 package com.carrental.CarListnerService.dto;
 
-public class BookingEvent {
+import lombok.*;
+import java.time.LocalDate;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BookingEvent {
+    private Long bookingId;
+    private Long carId;
+    private String customerName;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private double totalAmount;
 }
