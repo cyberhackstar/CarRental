4fcc64e778aafeaf13c10c0d7f73e2db71822362
cyberhackstar <64746305+cyberhackstar@users.noreply.github.com>
2025-06-03 01:27:26 +0530

    Update Booking.java

--- CarService/src/main/java/com/carrental/CarService/model/Booking.java
+++ CarService/src/main/java/com/carrental/CarService/model/Booking.java
@@ -1,13 +1,25 @@
 package com.carrental.CarService.model;
 
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Booking {
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private Long carId;
     private String customerName;
     private LocalDate startDate;
     private LocalDate endDate;
     private double totalAmount;
-    // Getters, Setters, Constructors
 }
