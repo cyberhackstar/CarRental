7445e99ecf2a4600de727db1323d7e28bd17a065
cyberhackstar <64746305+cyberhackstar@users.noreply.github.com>
2025-06-03 01:29:58 +0530

    Update CarRentalController.java

--- CarService/src/main/java/com/carrental/CarService/controller/CarRentalController.java
+++ CarService/src/main/java/com/carrental/CarService/controller/CarRentalController.java
@@ -1,5 +1,39 @@
 package com.carrental.CarService.controller;
 
+import com.car.service.model.Booking;
+import com.car.service.model.Car;
+import com.car.service.service.CarRentalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "*")
 public class CarRentalController {
 
+    @Autowired
+    private CarRentalService rentalService;
+
+    @GetMapping("/cars")
+    public List<Car> getAvailableCars() {
+        return rentalService.getAllAvailableCars();
+    }
+
+    @GetMapping("/cars/{id}")
+    public Optional<Car> getCarById(@PathVariable Long id) {
+        return rentalService.getCarById(id);
+    }
+
+    @PostMapping("/bookings")
+    public Booking bookCar(@RequestBody Booking booking) {
+        return rentalService.bookCar(booking);
+    }
+
+    @GetMapping("/bookings/{id}")
+    public Optional<Booking> getBooking(@PathVariable Long id) {
+        return rentalService.getBooking(id);
+    }
 }
