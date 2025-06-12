68e2e07eaa432148065d9de0d85ea00eb5757ec8
Bhawesh Sharma <bhawesh.10827643@ltimindtree.com>
2025-06-12 21:16:59 +0530

    listner service fix

--- CarListnerService/src/main/java/com/carrental/CarListnerService/CarListnerServiceApplication.java
+++ CarListnerService/src/main/java/com/carrental/CarListnerService/CarListnerServiceApplication.java
@@ -2,10 +2,9 @@ package com.carrental.CarListnerService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jms.annotation.EnableJms;
+
 
 @SpringBootApplication
-@EnableJms
 public class CarListnerServiceApplication {
 
 	public static void main(String[] args) {
