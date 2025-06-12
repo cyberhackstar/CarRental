e746c67061e2b0e20eebf10a97d1d7d12c4fb441
Bhawesh Sharma <bhawesh.10827643@ltimindtree.com>
2025-06-12 22:15:29 +0530

    rabbit fixed

--- CarService/src/main/java/com/carrental/CarService/RabitMQ/RabbitMQConfig.java
+++ CarService/src/main/java/com/carrental/CarService/RabitMQ/RabbitMQConfig.java
@@ -1,9 +1,13 @@
 package com.carrental.CarService.RabitMQ;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 
 @Configuration
 public class RabbitMQConfig {
@@ -22,4 +26,13 @@ public class RabbitMQConfig {
     public Queue bookingEventsQueue() {
         return new Queue("booking-events", true);
     }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
+            Jackson2JsonMessageConverter messageConverter) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter);
+        return template;
+    }
+
 }
